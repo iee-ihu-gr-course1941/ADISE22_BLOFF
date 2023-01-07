39b068f325431d31cfe9e7ef975a81c731ddda00
Stratos Kountouras <kountouras1998@gmail.com>
2023-01-07 22:09:35 +0200

    07/01/2023

--- lib/users.php
+++ lib/users.php
@@ -56,6 +56,20 @@ print json_encode($res->fetch_all(MYSQLI_ASSOC), JSON_PRETTY_PRINT);
      
 
 }
+function current_color($token) {
+	
+	global $mysqli;
+	if($token==null) {return(null);}
+	$sql = 'select * from players where token=?';
+	$st = $mysqli->prepare($sql);
+	$st->bind_param('s',$token);
+	$st->execute();
+	$res = $st->get_result();
+	if($row=$res->fetch_assoc()) {
+		return($row['player']);
+	}
+	return(null);
+}
 
 
 
